d65d87ed23dbd325bb4221c1e84b95881d46e7e4
Pavel Tikhonov <83917023+Fuithias@users.noreply.github.com>
2024-06-11 16:32:28 +0300

    Unqualified(mostly) translations

---
+++ items/buildscripts/thea-buildarmor.lua
@@ -0,0 +1,28 @@
+require "/scripts/util.lua"
+require "/scripts/versioningutils.lua"
+
+function build(directory, config, parameters, level, seed)
+  local configParameter = function(keyName, defaultValue)
+    if parameters[keyName] ~= nil then
+      return parameters[keyName]
+    elseif config[keyName] ~= nil then
+      return config[keyName]
+    else
+      return defaultValue
+    end
+  end
+  
+  config.tooltipFields = config.tooltipFields or {}
+  config.tooltipFields.levelLabel = util.round(configParameter("level", 1), 1)
+
+  --Редкость
+  if config.rarity == "common" or config.rarity == "Common" then config.tooltipFields.rarityLabel = "Обычный" end
+  if config.rarity == "uncommon" or config.rarity == "Uncommon" then config.tooltipFields.rarityLabel = "Необычный" end
+  if config.rarity == "rare" or config.rarity == "Rare" then config.tooltipFields.rarityLabel = "Редкий" end
+  if config.rarity == "legendary" or config.rarity == "Legendary" then config.tooltipFields.rarityLabel = "Легендарный" end
+  if config.rarity == "essential" or config.rarity == "Essential" then config.tooltipFields.rarityLabel = "Важный" end
+  --Тип предмета
+  if config.twoHanded then config.tooltipFields.handednessLabel = "2-Ручный" else config.tooltipFields.handednessLabel = "1-Ручный" end
+
+  return config, parameters
+end
---
+++ items/buildscripts/thea-buildattachment.lua
@@ -0,0 +1,19 @@
+function build(directory, config, parameters, level, seed)
+  --Populate the tooltip fields
+  config.tooltipFields = config.tooltipFields or {}
+  
+  local attachmentType = config.theaAttachmentType
+  config.tooltipFields.attachmentTypeImage = "/interface/attachmenttypes/"..attachmentType..".png"
+  config.tooltipFields.manufacturerLabel = config.manufacturer
+
+  --Редкость
+  if config.rarity == "common" or config.rarity == "Common" then config.tooltipFields.rarityLabel = "Обычный" end
+  if config.rarity == "uncommon" or config.rarity == "Uncommon" then config.tooltipFields.rarityLabel = "Необычный" end
+  if config.rarity == "rare" or config.rarity == "Rare" then config.tooltipFields.rarityLabel = "Редкий" end
+  if config.rarity == "legendary" or config.rarity == "Legendary" then config.tooltipFields.rarityLabel = "Легендарный" end
+  if config.rarity == "essential" or config.rarity == "Essential" then config.tooltipFields.rarityLabel = "Важный" end
+  --Тип предмета
+  if config.twoHanded then config.tooltipFields.handednessLabel = "2-Ручный" else config.tooltipFields.handednessLabel = "1-Ручный" end
+
+  return config, parameters
+end
